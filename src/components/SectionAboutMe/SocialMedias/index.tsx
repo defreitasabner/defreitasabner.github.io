7d854ca1da8c13abb91effdcbebcdca3ab7e56d0
Abner Freitas <defreitasabner@gmail.com>
2023-06-23 15:26:28 -0300

    Alterado nome do arquivo que contém as informações de mídias sociais.

--- src/components/SectionAboutMe/SocialMedias/index.tsx
+++ src/components/SectionAboutMe/SocialMedias/index.tsx
@@ -1,7 +1,7 @@
 import styles from './SocialMedias.module.scss';
 import SocialMediaBadge from './SocialMediaBadge';
 
-import socialMediaData from '../../../data/socialMedias.json';
+import socialMediaData from '../../../data/mySocialMedias.json';
 
 
 export default function SocialMedias() {
