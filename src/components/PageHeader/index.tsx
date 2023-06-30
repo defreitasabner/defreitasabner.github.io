c34072c620f01a49160db025c1b73c7a9283343f
Abner Freitas <defreitasabner@gmail.com>
2023-06-29 21:29:51 -0300

    Color pallete changes and other minor visual changes.

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,14 +4,7 @@ import styles from './PageHeader.module.scss';
 export default function PageHeader() {
     return (
         <header className={styles.header}>
-            <div>
-                <h1>Abner Freitas - Portfolio</h1>
-                <div data-menu-button>
-                    <div className="bar1"></div>
-                    <div className="bar2"></div>
-                    <div className="bar3"></div>
-                </div>
-            </div>
+            <h1>Abner Freitas - Portfolio</h1>
             <NavigationMenu />
         </header>
     )
