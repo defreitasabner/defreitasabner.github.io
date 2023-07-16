35fac7eb890f23255c7406e2e88eac769da8296c
Abner Freitas <defreitasabner@gmail.com>
2023-07-16 14:16:04 -0300

    Generated static page using next export

---
+++ out/_next/static/4UBSAqzzpyrn5SVrXb_pj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/375cd940d3b4503b.css","static/chunks/pages/index-b3a6f0d11470f390.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
