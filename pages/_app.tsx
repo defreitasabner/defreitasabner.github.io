fe03728e89ee9046cebc46a461bb3783922e72fc
Abner Freitas <defreitasabner@gmail.com>
2023-07-16 14:30:02 -0300

    Add tag Head to change page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import PageHeader from "../src/components/PageHeader";
 import PageFooter from "../src/components/PageFooter";
+import Head from "next/head";
 import "../src/styles/reset.css";
 import "../src/styles/style.scss";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>Abner Freitas</title>
+            </Head>
             <PageHeader />
             <Component { ...pageProps } />
             <PageFooter />
